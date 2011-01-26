c09733d7369b95b510edd6a814d409bd0f4d07db
Ole Petter Bang <olepbang@gmail.com>
2011-01-26 18:06:30 +0100

    Removed Octopussy, now searching for repositories directly.

--- projects.rb
+++ projects.rb
@@ -1,27 +1,31 @@
 require 'json'
-require 'octopussy'
+require 'uri'
+require 'net/http'
 require './feed'
 
 module Projects
+  URL = 'http://github.com/api/v2/json/repos/search/username:gnab'
+
   def self.get_updated_at(entry)
-    datetime = DateTime.parse(entry[:pushed_at]).to_time
+    datetime = DateTime.parse(entry['pushed_at']).to_time
     Feed.format_datetime(datetime)
   end
 
   def self.retrieve
-    entries = Octopussy::Client.new.list_repos('gnab')  
-  
+    data = Net::HTTP.get_response(URI.parse(URL)).body
+    entries = JSON.parse(data)['repositories']
+
     projects = entries.sort_by {|e| e[:pushed_at]}.reverse.collect do |entry|
       {
-        :name => entry[:name],
-        :description => entry[:description],
-        :url => entry[:url],
-        :created_at => entry[:created_at],
+        :name => entry['name'],
+        :description => entry['description'],
+        :url => entry['url'],
+        :created_at => entry['created_at'],
         :updated_at => get_updated_at(entry),
-        :language => entry[:language],
-        :url => entry[:url],
-        :watchers => entry[:watchers],
-        :forks => entry[:forks]
+        :language => entry['language'],
+        :url => entry['url'],
+        :watchers => entry['watchers'],
+        :forks => entry['forks']
       }
     end
 
