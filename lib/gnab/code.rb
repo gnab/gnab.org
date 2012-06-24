970dbaaefeb611b82db24851850d9477559bc3ac
Ole Petter Bang <olepbang@gmail.com>
2012-06-24 18:57:35 +0200

    Fix retrieval of Github repos.

--- lib/gnab/code.rb
+++ lib/gnab/code.rb
@@ -1,21 +1,29 @@
 require 'json'
 require 'uri'
 require 'net/http'
+require 'net/https'
 require 'gnab/common'
 
 module Gnab::Code
   extend Gnab::Common
 
-  URL = 'http://github.com/api/v2/json/repos/search/username:gnab'
-  FIELDS = [:name, :description, :url, :pushed_at, :language, :url, :watchers, 
-            :forks] 
+  FIELDS = [:name, :description, :pushed_at, :language, :watchers, :forks,
+            :html_url => :url]
 
   def self.retrieve
-    data = Net::HTTP.get_response(URI.parse(URL)).body
-    
-    repos = JSON.parse(data)['repositories'].collect do |entry|
+    http = Net::HTTP.new('api.github.com', 443)
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http.use_ssl = true
+    data = http.request(Net::HTTP::Get.new('/users/gnab/repos')).body
+
+    repos = JSON.parse(data).collect do |entry|
       FIELDS.inject({}) do |repo, field|
-        repo[field] = entry[field.to_s]
+        if field.is_a?(Hash)
+          from, to = field.first
+          repo[to] = entry[from.to_s]
+        else
+          repo[field] = entry[field.to_s]
+        end
         repo[:kind] = :project
         repo
       end
